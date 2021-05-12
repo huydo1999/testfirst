fc8b9954abe09811ac164f7fe5c9b2d2dcd87931
HuyDo <huydo1999dn@gmail.com>
2021-05-12 15:08:51 +0700

    add new project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=8;
+    int b=10;
+    int c;
+    c = a+b;
+    cout<<"gia tri cua a="<<a<<endl;
+    cout<<"gia tri cua b="<<b<<endl;
+    cout<<"gia tri cua c="<<c<<endl;
+
+    return 0;
+}
